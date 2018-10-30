7d43613793b71865a4a431239a169a3d50a5903a
Shruti Remadevi <rshruti0307@gmail.com>
2018-10-29 22:03:43 -0400

    Server-side rendering actions and reducers

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
 	return (
 		<div>
-			<div>I am the dnsbnvjk Home component</div>
+			<div>I am the Home component</div>
 			<button onClick={() => console.log("button clicked")}>Click me!</button>
 		</div>
 		);
--- webpack.client.js
+++ webpack.client.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
 
-module.exports = {
+const config = {
 	// server-application entry point
 	entry: './src/client/client.js',
 
@@ -8,23 +10,7 @@ module.exports = {
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'public')
-	},
-
-	// run babel on every file
-	module: {
-		rules: [
-			{
-				test: /\.js?$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-				options: {
-					presets: [
-						'react',
-						'stage-0',
-						['env', { targets: { browsers: ['last 2 versions'] } }]
-					]
-				}
-			}
-		]
 	}
 };
+
+module.exports = merge(baseConfig, config);
